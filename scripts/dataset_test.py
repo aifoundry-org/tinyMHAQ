8827f2e6d46d05af200f6458f01e39e8b3b6bddd
janchk <janchk@gmail.com>
2025-02-25 21:39:18 +0000

    Fix: dataset test example

--- scripts/dataset_test.py
+++ scripts/dataset_test.py
@@ -8,7 +8,6 @@ from src.data.datasets.cifar10 import Cifar10Dataset
 
 if __name__ == "__main__":
     dataset = Cifar10Dataset(data_dir=os.path.dirname(os.path.dirname(os.path.realpath(__file__))), batch_size=64)
-    # print(next(dataset.get_test_batch()))
-    for i in trange(dataset.get_test_dataloader()):
-        pass
-    # print(next(dataset.get_train_batch()))
+    dataloader = dataset.get_test_dataloader()
+    for i in trange(len(dataloader)):
+        next(dataloader)
